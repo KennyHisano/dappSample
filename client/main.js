79568e7b5cb45fc2eb30fb222659c9e975954922
NandN <NandN@inandn.local>
2017-05-15 13:13:00 +0900

    ahah

---
+++ client/main.js
@@ -0,0 +1,25 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
+});
+
+Template.hello.helpers({
+  counter() {
+    web3.eth.getBalance("0x1c9b458705FccEdDDB631653361c096bd3Eec020",
+    	function (err, res){
+    		alert(res);
+    })
+  },
+});
+
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
